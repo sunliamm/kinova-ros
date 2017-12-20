569142a9515882dabfa3604d2bcc6a04731007f6
David C. Conner <david.conner@cnu.edu>
2017-12-20 15:44:26 -0500

    kinetic - use optenv to set KINOVA_ARM type; add _driver to controller namespace; add moveit controller setup for m1n6s200

--- kinova_control/src/move_robot.py
+++ kinova_control/src/move_robot.py
@@ -15,56 +15,56 @@ def argumentParser(argument):
   argv = rospy.myargv()
   args_ = parser.parse_args(argv[1:])
   prefix = args_.kinova_robotType
-  nbJoints = int(args_.kinova_robotType[3])	
-  nbfingers = int(args_.kinova_robotType[5])	
+  nbJoints = int(args_.kinova_robotType[3])
+  nbfingers = int(args_.kinova_robotType[5])
   return prefix, nbJoints, nbfingers
 
 def moveJoint (jointcmds,prefix,nbJoints):
-  topic_name = '/' + prefix + '/effort_joint_trajectory_controller/command'
+  topic_name = '/' + prefix + '_driver/effort_joint_trajectory_controller/command'
   pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
-  jointCmd = JointTrajectory()  
+  jointCmd = JointTrajectory()
   point = JointTrajectoryPoint()
-  jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);  
+  jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);
   point.time_from_start = rospy.Duration.from_sec(5.0)
   for i in range(0, nbJoints):
     jointCmd.joint_names.append(prefix +'_joint_'+str(i+1))
     point.positions.append(jointcmds[i])
     point.velocities.append(0)
     point.accelerations.append(0)
-    point.effort.append(0) 
+    point.effort.append(0)
   jointCmd.points.append(point)
   rate = rospy.Rate(100)
   count = 0
   while (count < 50):
     pub.publish(jointCmd)
     count = count + 1
-    rate.sleep()     
+    rate.sleep()
 
 def moveFingers (jointcmds,prefix,nbJoints):
-  topic_name = '/' + prefix + '/effort_finger_trajectory_controller/command'
-  pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)  
-  jointCmd = JointTrajectory()  
+  topic_name = '/' + prefix + '_driver/effort_finger_trajectory_controller/command'
+  pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
+  jointCmd = JointTrajectory()
   point = JointTrajectoryPoint()
-  jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);  
+  jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);
   point.time_from_start = rospy.Duration.from_sec(5.0)
   for i in range(0, nbJoints):
     jointCmd.joint_names.append(prefix +'_joint_finger_'+str(i+1))
     point.positions.append(jointcmds[i])
     point.velocities.append(0)
     point.accelerations.append(0)
-    point.effort.append(0) 
+    point.effort.append(0)
   jointCmd.points.append(point)
   rate = rospy.Rate(100)
   count = 0
   while (count < 500):
     pub.publish(jointCmd)
     count = count + 1
-    rate.sleep()     
+    rate.sleep()
 
 if __name__ == '__main__':
-  try:    
-    rospy.init_node('move_robot_using_trajectory_msg')		
-    prefix, nbJoints, nbfingers = argumentParser(None)    
+  try:
+    rospy.init_node('move_robot_using_trajectory_msg')
+    prefix, nbJoints, nbfingers = argumentParser(None)
     #allow gazebo to launch
     rospy.sleep(1)
 
