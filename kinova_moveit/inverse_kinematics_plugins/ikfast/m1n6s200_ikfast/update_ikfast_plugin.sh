23f1d8f06a76988eca5ea17f0d0e404e87aae625
David C. Conner <david.conner@cnu.edu>
2017-12-20 12:00:48 -0500

    kinetic - add m1n6s200 package using driver/arm_controller name space

---
+++ kinova_moveit/inverse_kinematics_plugins/ikfast/m1n6s200_ikfast/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py m1n6s200 mico_arm m1n6s200_ikfast /home/conner/KinovaMerge/src/kinova-ros-devel/kinova_moveit/inverse_kinematics_plugins/ikfast/m1n6s200_ikfast/src/m1n6s200_mico_arm_ikfast_solver.cpp
